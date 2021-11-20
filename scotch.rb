5295d03149c3f1b3f1c94c78972f69de56180e77
Guanyang Xue <playlego@gmail.com>
2021-11-20 13:41:21 -0500

    Update scotch.rb

--- scotch.rb
+++ scotch.rb
@@ -1,8 +1,8 @@
 class Scotch < Formula
   desc "Package for graph partitioning, graph clustering, and sparse matrix ordering"
   homepage "https://gitlab.inria.fr/scotch/scotch"
-  url "https://gitlab.inria.fr/scotch/scotch/-/archive/v6.1.1/scotch-v6.1.1.tar.bz2"
-  sha256 "543ab2f8998658e5a6123231bfb3736cfbb939f330c656f8a7913d967a933f00"
+  url "https://gitlab.inria.fr/scotch/scotch/-/archive/v6.1.2/scotch-v6.1.2.tar.bz2"
+  sha256 "6e820a64cc2105749e3d1dfbfc9aed33597a85927b6f56d073a6ef602724ea2d"
   license "CECILL-C"
   head "https://gitlab.inria.fr/scotch/scotch.git", branch: "master"
 
@@ -18,18 +18,28 @@ class Scotch < Formula
   uses_from_macos "zlib"
 
   def install
-    cd "src"
-    (buildpath/"src").install_symlink "Make.inc/Makefile.inc.i686_mac_darwin10" => "Makefile.inc"
-    system "make", "scotch"
-    system "make", "ptscotch"
-    system "make", "prefix=#{prefix}", "install"
+    cd "src" do
+      (buildpath/"src").install_symlink "Make.inc/Makefile.inc.i686_mac_darwin10" => "Makefile.inc"
+      inreplace "Makefile.inc" do |s|
+        s.change_make_var! "CCS", ENV.cc
+        s.change_make_var! "CCP", "mpicc"
+        s.change_make_var! "CCD", "mpicc"
+        s.gsub! "-DSCOTCH_PTHREAD", ""
+      end
+      
+
+      system "make", "scotch", "ptscotch"
+      system "make", "prefix=#{prefix}", "install"
+
+      pkgshare.install "check/test_strat_seq.c"
+      pkgshare.install "check/test_strat_par.c"
+    end
   end
 
   test do
     (testpath/"test.c").write <<~EOS
       #include <stdlib.h>
       #include <stdio.h>
-
       #include <scotch.h>
       int main(void) {
         int major, minor, patch;
@@ -38,8 +48,15 @@ class Scotch < Formula
         return 0;
       }
     EOS
-
     system ENV.cc, "test.c", "-L#{lib}", "-lscotch"
     assert_match version.to_s, shell_output("./a.out")
+
+    system ENV.cc, pkgshare/"test_strat_seq.c", "-o", "test_strat_seq",
+           "-I#{include}", "-L#{lib}", "-lscotch", "-lscotcherr", "-lm", "-pthread"
+    assert_match "Sequential mapping strategy, SCOTCH_STRATDEFAULT", shell_output("./test_strat_seq")
+
+    system "mpicc", pkgshare/"test_strat_par.c", "-o", "test_strat_par",
+           "-I#{include}", "-L#{lib}", "-lptscotch", "-lscotch", "-lptscotcherr", "-lm", "-pthread"
+    assert_match "Parallel mapping strategy, SCOTCH_STRATDEFAULT", shell_output("./test_strat_par")
   end
 end
