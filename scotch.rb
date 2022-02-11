d8084431ecdb985f5c8e0898ca38d3b5e285e3a1
Guanyang Xue <playlego@gmail.com>
2022-02-10 20:14:45 -0500

    Add files via upload
    7.0.0

--- scotch.rb
+++ scotch.rb
@@ -1,8 +1,8 @@
 class Scotch < Formula
   desc "Package for graph partitioning, graph clustering, and sparse matrix ordering"
   homepage "https://gitlab.inria.fr/scotch/scotch"
-  url "https://gitlab.inria.fr/scotch/scotch/-/archive/v6.1.2/scotch-v6.1.2.tar.bz2"
-  sha256 "6e820a64cc2105749e3d1dfbfc9aed33597a85927b6f56d073a6ef602724ea2d"
+  url "https://gitlab.inria.fr/scotch/scotch/-/archive/v7.0.0/scotch-v7.0.0.tar.bz2"
+  sha256 "a36a26c89b6eeb0ffcda8378fb29dd98a2589a47be93c67b59e00374c878054a"
   license "CECILL-C"
   head "https://gitlab.inria.fr/scotch/scotch.git", branch: "master"
 
@@ -11,22 +11,53 @@ class Scotch < Formula
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    sha256 cellar: :any,                 arm64_monterey: "26b950506f3fe8caa735b151641771956d5a138bc31babbf10e32e2f6308b99a"
+    sha256 cellar: :any,                 arm64_big_sur:  "93af205f1f99e2520e4979b305052bb2407ae3b22f20f9f81c427ccb487b6fe0"
+    sha256 cellar: :any,                 monterey:       "374db0a88ff61869fd53bd3f308bff9b4d1bdb22477182563137f3e38974fe67"
+    sha256 cellar: :any,                 big_sur:        "94f374c1faadfa24aeb7bab6b3b709c257283ce451ae9a982491f81685f38e83"
+    sha256 cellar: :any,                 catalina:       "526cd44e60be24309806b8a73d035bc51af69748ab6dc0a5e1ddaea483e2189e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "23bb7fc89073b21386ce427867b49c25f0898dc854641fb6f6cf20ed1d96b29d"
+  end
+
+  depends_on "bison" => :build
   depends_on "open-mpi"
 
-  uses_from_macos "bison"
-  uses_from_macos "flex"
+  uses_from_macos "flex" => :build
   uses_from_macos "zlib"
 
   def install
+    makefile_inc_suffix = OS.mac? ? "i686_mac_darwin10" : "x86-64_pc_linux2"
+    (buildpath/"src").install_symlink "Make.inc/Makefile.inc.#{makefile_inc_suffix}" => "Makefile.inc"
+
     cd "src" do
-      (buildpath/"src").install_symlink "Make.inc/Makefile.inc.i686_mac_darwin10" => "Makefile.inc"
-      inreplace "Makefile.inc" do |s|
+      inreplace_files = ["Makefile.inc"]
+      inreplace_files << "Make.inc/Makefile.inc.#{makefile_inc_suffix}.shlib" unless OS.mac?
+
+      inreplace inreplace_files do |s|
         s.change_make_var! "CCS", ENV.cc
         s.change_make_var! "CCP", "mpicc"
         s.change_make_var! "CCD", "mpicc"
         s.gsub! "-DSCOTCH_PTHREAD", ""
       end
-      
+
+      system "make", "libscotch", "libptscotch"
+      lib.install buildpath.glob("lib/*.a")
+      system "make", "realclean"
+
+      # Build shared libraries. See `Makefile.inc.*.shlib`.
+      if OS.mac?
+        inreplace "Makefile.inc" do |s|
+          s.change_make_var! "LIB", ".dylib"
+          s.change_make_var! "AR", ENV.cc
+          s.change_make_var! "ARFLAGS", "-shared -Wl,-undefined,dynamic_lookup -o"
+          s.change_make_var! "CLIBFLAGS", "-shared -fPIC"
+          s.change_make_var! "RANLIB", "true"
+        end
+      else
+        Pathname("Makefile.inc").unlink
+        ln_sf "Make.inc/Makefile.inc.#{makefile_inc_suffix}.shlib", "Makefile.inc"
+      end
 
       system "make", "scotch", "ptscotch"
       system "make", "prefix=#{prefix}", "install"
@@ -34,6 +65,10 @@ class Scotch < Formula
       pkgshare.install "check/test_strat_seq.c"
       pkgshare.install "check/test_strat_par.c"
     end
+
+    # License file has a non-standard filename
+    prefix.install buildpath.glob("LICEN[CS]E_*.txt")
+    doc.install (buildpath/"doc").children
   end
 
   test do
@@ -48,15 +83,18 @@ class Scotch < Formula
         return 0;
       }
     EOS
-    system ENV.cc, "test.c", "-L#{lib}", "-lscotch"
+    system ENV.cc, "test.c", "-L#{lib}", "-lscotch", "-lscotcherr",
+                             "-pthread", "-L#{Formula["zlib"].opt_lib}", "-lz", "-lm"
     assert_match version.to_s, shell_output("./a.out")
 
     system ENV.cc, pkgshare/"test_strat_seq.c", "-o", "test_strat_seq",
-           "-I#{include}", "-L#{lib}", "-lscotch", "-lscotcherr", "-lm", "-pthread"
+                   "-I#{include}", "-L#{lib}", "-lscotch", "-lscotcherr", "-lm", "-pthread",
+                   "-L#{Formula["zlib"].opt_lib}", "-lz"
     assert_match "Sequential mapping strategy, SCOTCH_STRATDEFAULT", shell_output("./test_strat_seq")
 
     system "mpicc", pkgshare/"test_strat_par.c", "-o", "test_strat_par",
-           "-I#{include}", "-L#{lib}", "-lptscotch", "-lscotch", "-lptscotcherr", "-lm", "-pthread"
+                    "-I#{include}", "-L#{lib}", "-lptscotch", "-lscotch", "-lptscotcherr", "-lm", "-pthread",
+                    "-L#{Formula["zlib"].opt_lib}", "-lz", "-Wl,-rpath,#{lib}"
     assert_match "Parallel mapping strategy, SCOTCH_STRATDEFAULT", shell_output("./test_strat_par")
   end
 end
